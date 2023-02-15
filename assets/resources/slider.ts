3d85f799e2a167566fd62659be4aaf61ec2e7081
Aryan Khanna <aryan.khanna@chicmic.co.in>
2023-02-15 17:16:39 +0530

    fixing repo

---
+++ assets/resources/slider.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('slider')
+export class slider extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
--- assets/scripts/constants.ts
+++
@@ -1,5 +0,0 @@
-export enum PUCKS {
-  RED = 1,
-  BLACK = 2,
-  WHITE = 3,
-}
